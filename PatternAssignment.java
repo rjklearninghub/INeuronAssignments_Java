4569ee48bfcf6c2256310df3b1985229cb2c7453
rjklearninghub <97813331+rjklearninghub@users.noreply.github.com>
2022-07-29 20:55:00 +0100

    Add files via upload

---
+++ PatternAssignment.java
@@ -0,0 +1,14 @@
+package learn.ineuron.ai;
+
+public class PatternAssignment {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=4; i++) {
+			for (int j = 1; j<=4;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
